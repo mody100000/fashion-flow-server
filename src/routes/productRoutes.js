7186c8a68d837ba5467dd6235b8e249cb09b7591
mody100000 <m.anamody10000@gmail.com>
2023-02-27 00:56:28 +0200

    modifying the product CRUD

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,15 +1,18 @@
 const { Router } = require('express')
 const productController = require('../controllers/productController')
-const {
-  getAllProducts,
-  getProduct,
-  deleteProduct,
-} = require('../services/productService')
+// const {
+//   getAllProducts,
+//   getProduct,
+//   deleteProduct,
+// } = require('../services/productService')
 
 const router = Router()
 
 router.post('/', productController.createProduct)
-router.get('/', getAllProducts)
-router.get('/:id', getProduct)
-router.delete('/:id', deleteProduct)
+router.get('/', productController.products)
+router.get('/:id', productController.product)
+router.delete('/:id', productController.deleteProduct)
+// router.get('/', getAllProducts)
+// router.get('/:id', getProduct)
+// router.delete('/:id', deleteProduct)
 module.exports = router
