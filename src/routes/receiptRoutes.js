d89f62fba60e220e5b8ad03f66acebc0078ba7a1
mody100000 <m.anamody10000@gmail.com>
2023-03-02 21:45:16 +0200

    finshing receit CRUD

--- src/routes/receiptRoutes.js
+++ src/routes/receiptRoutes.js
@@ -3,4 +3,7 @@ const router = Router()
 const receiptController = require('../controllers/receiptController')
 
 router.post('/', receiptController.createReceipt)
+router.get('/', receiptController.receipts)
+router.get('/:id', receiptController.receipt)
+router.delete('/:id', receiptController.deleteReceipt)
 module.exports = router
