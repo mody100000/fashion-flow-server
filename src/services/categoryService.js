92f9ca721379746c225fe051d81c051caacc1de9
mody100000 <m.anamody10000@gmail.com>
2023-03-05 20:57:24 +0200

    whatever v2

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,6 +1,8 @@
 const Category = require('../models/Category')
+const _ = require('lodash')
 const errorHandler = require('../utils/errorHandler')
 const isValidObjectId = require('../utils/isValidObjectId')
+const moment = require('moment')
 
 const createCategory = async input => {
     const createdCategory = await Category.create(input)
@@ -25,9 +27,25 @@ const deleteCategory = async id => {
     if (!deletedCategory) return null
     return deletedCategory
 }
+const categoryReport = async options => {
+    const finalMonths = options.monthsBefore || 5
+    // TODO: think about caching this
+
+    return await Promise.all(
+        _.range(finalMonths).map(async num => {
+            const date = moment().subtract(finalMonths - num - 1, 'months')
+            const categories = await Category.find({
+                createdAt: { $lte: date },
+            })
+            console.log(categories)
+            return categories
+        })
+    )
+}
 module.exports = {
     createCategory,
     getAllCategories,
     getCategory,
     deleteCategory,
+    categoryReport,
 }
